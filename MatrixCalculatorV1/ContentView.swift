5e30708f68e7ab5ba952d2a3f68e5008070111ea
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-12-22 19:53:47 -0600

    Initial Commit

---
+++ MatrixCalculatorV1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MatrixCalculatorV1
+//
+//  Created by Abdullah Abdulkareem on 12/22/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MatrixCalculatorV1/MatrixCalculatorV1App.swift
@@ -0,0 +1,17 @@
+//
+//  MatrixCalculatorV1App.swift
+//  MatrixCalculatorV1
+//
+//  Created by Abdullah Abdulkareem on 12/22/24.
+//
+
+import SwiftUI
+
+@main
+struct MatrixCalculatorV1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
