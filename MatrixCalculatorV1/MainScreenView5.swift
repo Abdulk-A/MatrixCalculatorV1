574d6c4e6a4f08db01d23d60133bdf98d3bb9553
Abdulk-A <abdullahabdulkareem273@gmail.com>
2024-12-26 01:45:38 -0600

    added more rough drafts for main screen

---
+++ MatrixCalculatorV1/MainScreenView5.swift
@@ -0,0 +1,97 @@
+//
+//  MainScreen5.swift
+//  MatrixCalculatorV1
+//
+//  Created by Abdullah Abdulkareem on 12/26/24.
+//
+
+import SwiftUI
+
+struct MainScreenView5: View {
+    var body: some View {
+        ZStack {
+            Color.black.opacity(0.25)
+            VStack {
+                Text("Menu")
+                    .frame(maxWidth: .infinity)
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
+                    .blur(radius: 8)
+                    .overlay{
+                        Text("Menu")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                            .bold()
+                    }
+                    
+
+                
+                VStack {
+                    Button {
+                        
+                    } label: {
+                        Text("Calculator")
+                    }
+                    .buttonStyle(ExampleButton6())
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Practice")
+                    }
+                    .buttonStyle(ExampleButton6())
+                    
+                    
+                    Button {
+                        
+                    } label: {
+                        Text("Feedback")
+                    }
+                    .buttonStyle(ExampleButton6())
+
+                }
+                .padding()
+            }
+            .frame(maxWidth: 500)
+            .padding(.vertical)
+            .background(
+                RoundedRectangle(cornerRadius: 15)
+                    .foregroundStyle(.gray.opacity(0.5))
+                    .shadow(radius: /*@START_MENU_TOKEN@*/10/*@END_MENU_TOKEN@*/)
+            )
+            .padding()
+            
+        }
+        .ignoresSafeArea(.all)
+        
+    }
+}
+
+#Preview {
+    MainScreenView5()
+}
+
+struct ExampleButton6: ButtonStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            
+            .padding()
+        
+            .frame(maxWidth: .infinity)
+            .background(
+                RoundedRectangle(cornerRadius: 10)
+                    .foregroundStyle(.black.opacity(0.6))
+            )
+            .font(.title.bold())
+            
+
+            .foregroundStyle(.white.opacity(0.8))
+            .padding(.bottom, 6)
+            .shadow(radius: 5)
+            .scaleEffect(configuration.isPressed ? 1.1 : 1)
+            .animation(.easeOut(duration: 0.2), value: configuration.isPressed)
+            .shadow(radius: 6)
+            
+    }
+}
