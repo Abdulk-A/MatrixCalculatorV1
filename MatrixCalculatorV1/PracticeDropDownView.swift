2d009620c4dff3630c186cdf10064575d1c15bf0
Abdulk-A <abdullahabdulkareem273@gmail.com>
2025-03-10 23:59:23 -0500

    ADD: staircase animation for dropdown menu

--- MatrixCalculatorV1/PracticeDropDownView.swift
+++ MatrixCalculatorV1/PracticeDropDownView.swift
@@ -10,50 +10,77 @@ import SwiftUI
 struct PracticeDropDownView: View {
     
     @State private var showOtherFeatures = false
+    @State private var f1 = false
+    @State private var f2 = false
+    @State private var f3 = false
     
     var body: some View {
-//        Button(isListForm ? "Matrix" : "List") {
-//            withAnimation {
-//                isListForm.toggle()
-//            }
-//        }
-//        .frame(width: 75)
-//        .padding([.trailing, .vertical], 8)
-//        .background(Color("ButtonBackgroundStyle"))
-//        .clipShape(RoundedRectangle(cornerRadius: 10))
-//        .foregroundStyle(.white)
-        
-        
-        HStack(spacing: 5) {
-            
-            Button("Something") {
-                
-            }
-            .SomeModifer(for: 100)
-            
 
-            Image(systemName: showOtherFeatures ? "chevron.up": "chevron.down")
-                .onTapGesture {
-                    withAnimation {
-                        showOtherFeatures.toggle()
-                    }
-                }
-                .frame(width: 25)
-        }
         
         VStack {
-            if showOtherFeatures {
-                Button("one") {}
-                    .SomeModifer(for: 130)
-                Button("two") {}
-                    .SomeModifer(for: 130)
-                Button("three") {}
-                    .SomeModifer(for: 130)
+            HStack(spacing: 5) {
+                
+                Button("Something") {
+                    
+                }
+                .SomeModifer(for: 100)
+                
+
+                Image(systemName: showOtherFeatures ? "chevron.up": "chevron.down")
+                    .onTapGesture {
+                        withAnimation {
+                            showOtherFeatures.toggle()
+                        }
+                        
+                        if !showOtherFeatures {
+                            f1 = false
+                            f2 = false
+                            f3 = false
+                        } else {
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                                f1 = true
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                                f2 = true
+                            }
+                            DispatchQueue.main.asyncAfter(deadline: .now() + 0.3) {
+                                f3 = true
+                            }
+                        }
+                    }
+                    .frame(width: 25)
             }
+            
+            
+            if showOtherFeatures {
+                VStack {
+                    if f1 {
+                        Button("one") {}
+                            .SomeModifer(for: 130)
+                            .transition(.move(edge: .top))
+                    }
+                    if f2 {
+                        Button("two") {}
+                            .SomeModifer(for: 130)
+                            .transition(.move(edge: .top))
+                    }
+
+                    if f3 {
+                        Button("three") {}
+                            .SomeModifer(for: 130)
+                            .transition(.move(edge: .top))
+                    }
+                }
+                .padding(.top, 10)
+                .frame(height: 130)
+            } else {
+                Rectangle()
+                    .opacity(0)
+                    .frame(height: 130)
+            }
+              
         }
-        .padding(.top, 10)
-        
-        
+        .frame(maxHeight: 300)
     }
 }
 
